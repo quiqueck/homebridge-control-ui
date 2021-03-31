097d478ad52a9e2438d01124b514910d2338dae1
Frank Bauer <developer@ambertation.de>
2021-03-31 12:48:55 +0200

    test script

---
+++ __dev/Api/src/serve.ts
@@ -0,0 +1,23 @@
+import { HapListener } from './lib/Listener'
+import { Server } from './lib/Server'
+
+const server = new Server({
+    port: 8099,
+    baseURL: '',
+})
+server.start()
+
+const listener = new HapListener({
+    pin: '178-12-920',
+    //pin: '121-13-920',
+    logger: {
+        log: function (a: any) {
+            console.log('l', a)
+        },
+        warn: function (a: any) {
+            console.log('w', a)
+        },
+    },
+    config: { debug: true },
+})
+listener.start()
